a039b3628e21cdd257b5d12fddb005dd75a210a7
Mitchell Jonker <mcjonker9@gmail.com>
2021-11-05 00:10:04 -0400

    added more tests

--- StringHelper/StringHelperTester.java
+++ StringHelper/StringHelperTester.java
@@ -6,14 +6,9 @@ import java.util.Scanner;
 public class StringHelperTester {
 
 	public static void main(String[] args) {
-		//public static StringHelper helper = new StringHelper();
-		
-		//Scanner scanner = new Scanner(System.in);
-		
+
 		System.out.println("Welcome to String Helper Tester!");
-		
-		//String input = scanner.nextLine();
-		//String input2 = scanner.nextLine();
+
 		System.out.println("\nTesting the Has All Unique Characters method with \"Water\"");
 		System.out.println(StringHelper.hasAllUniqueCharaters("Water"));
 		System.out.println("\nTesting the Has All Unique Characters method with \"Applebees\"");
@@ -21,17 +16,18 @@ public class StringHelperTester {
 		
 		System.out.println("\nTesting the Mesh Strings method with \"orange\" and \"guitar\"");
 		System.out.println(StringHelper.meshStrings("orange", "guitar"));
+		System.out.println("\nTesting the Mesh Strings method with \"alphabet\" and \"texas\"");
+		System.out.println(StringHelper.meshStrings("alphabet", "texas"));
 		
 		System.out.println("\nTesting the Replace Vowels with Oodle method with \"Noodle\"");
 		System.out.println(StringHelper.replaceVowelsWithOodle("Noodle"));
+		System.out.println("\nTesting the Replace Vowels with Oodle method with \"Massachusetts\"");
+		System.out.println(StringHelper.replaceVowelsWithOodle("Massachusetts"));
 		
 		System.out.println("\nTesting the Weight method with \"Massive\"");
 		System.out.println(StringHelper.weight("Massive"));
-		
-		
-		
-		
-		System.out.println();
+		System.out.println("\nTesting the Weight method with \"Axel\"");
+		System.out.println(StringHelper.weight("Axel"));
 	}
 
 }
